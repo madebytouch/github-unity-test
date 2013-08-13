6579fdbc4b942bf9b50198d9e563a21603b5845d
madebytouch <andy@madebytouch.co.uk>
2013-08-13 17:15:38 +0100

    Initial Commit
    First Push!

---
+++ Github/Assets/Scripts/enemyBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyBehaviour : MonoBehaviour {
+
+	private string enemyName = "Charles";
+	private int health = 100;
+	private float height = 5.8f;
+
+}
