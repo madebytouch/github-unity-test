19c209f2893f5defdd2f343792792100b1f553a8
madebytouch <andy@madebytouch.co.uk>
2013-08-13 17:31:42 +0100

    Added a random picker for enemy health

--- Github/Assets/Scripts/enemyBehaviour.cs
+++ Github/Assets/Scripts/enemyBehaviour.cs
@@ -4,7 +4,25 @@ using System.Collections;
 public class enemyBehaviour : MonoBehaviour {
 
 	private string enemyName = "Craig";
-	private int health = 100;
+	private int enemyHealth;
 	private float height = 5.8f;
+
+	private string[] gunArray;
+	private string currentGun;
+
+	void Start(){
+		
+		enemyHealth = pickHealth(40, 150);
+
+		Debug.Log(enemyHealth);
+
+	}
+
+	public int pickHealth(int minHealth, int maxHealth){
+
+		int randomHealth = Random.Range(minHealth, maxHealth);
+
+		return randomHealth;
+	}
 
 }
